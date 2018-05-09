f4aa2aaa762282fd50145fdddeafee82aaa207c7
Prasenjeet <prasenjeet@bk.ru>
2018-05-09 22:30:49 +0530

    adding new files

--- src/app/home/main-page/main-page.component.ts
+++ src/app/home/main-page/main-page.component.ts
@@ -1,4 +1,6 @@
+import { NewPeopleUpdateComponent } from './../new-people-update/new-people-update.component';
 import { Component, OnInit } from '@angular/core';
+import { SignUpComponent } from '../sign-up/sign-up.component';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
+
+  listOfPeople: ListOfPeople[] = [
+    {
+      profilePicture : '../../../assets/profilePicture/teenp1.jpg',
+      nameOfPerson : 'Harry Potter',
+      timeOfJoining: 2
+    },
+    {
+      profilePicture : '../../../assets/profilePicture/teenp1.jpg',
+      nameOfPerson : 'Harry Potter',
+      timeOfJoining: 2
+    },
+    {
+      profilePicture : '../../../assets/profilePicture/teenp1.jpg',
+      nameOfPerson : 'Harry Potter',
+      timeOfJoining: 2
+    },
+    {
+      profilePicture : '../../../assets/profilePicture/teenp1.jpg',
+      nameOfPerson : 'Harry Potter',
+      timeOfJoining: 2
+    },
+    {
+      profilePicture : '../../../assets/profilePicture/teenp1.jpg',
+      nameOfPerson : 'Harry Potter',
+      timeOfJoining: 2
+    },
+    {
+      profilePicture : '../../../assets/profilePicture/teenp1.jpg',
+      nameOfPerson : 'Harry Potter',
+      timeOfJoining: 2
+    },
+    {
+      profilePicture : '../../../assets/profilePicture/teenp1.jpg',
+      nameOfPerson : 'Harry Potter',
+      timeOfJoining: 2
+    }
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+
+
+interface ListOfPeople {
+  profilePicture: string;
+  nameOfPerson: string;
+  timeOfJoining: number;
 }
--- src/app/home/new-people-update/new-people-update.component.ts
+++ src/app/home/new-people-update/new-people-update.component.ts
@@ -7,9 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewPeopleUpdateComponent implements OnInit {
 
+  listOfPeople: ListOfPeople[] = [
+    {
+      profilePicture : '../../../assets/profilePicture/teenp1.jpg',
+      nameOfPerson : 'Harry Potter',
+      timeOfJoining: 2
+    },
+    {
+      profilePicture : '../../../assets/profilePicture/teenp1.jpg',
+      nameOfPerson : 'Harry Potter',
+      timeOfJoining: 2
+    },
+    {
+      profilePicture : '../../../assets/profilePicture/teenp1.jpg',
+      nameOfPerson : 'Harry Potter',
+      timeOfJoining: 2
+    },
+    {
+      profilePicture : '../../../assets/profilePicture/teenp1.jpg',
+      nameOfPerson : 'Harry Potter',
+      timeOfJoining: 2
+    },
+    {
+      profilePicture : '../../../assets/profilePicture/teenp1.jpg',
+      nameOfPerson : 'Harry Potter',
+      timeOfJoining: 2
+    },
+    {
+      profilePicture : '../../../assets/profilePicture/teenp1.jpg',
+      nameOfPerson : 'Harry Potter',
+      timeOfJoining: 2
+    },
+    {
+      profilePicture : '../../../assets/profilePicture/teenp1.jpg',
+      nameOfPerson : 'Harry Potter',
+      timeOfJoining: 2
+    },
+    {
+      profilePicture : '../../../assets/profilePicture/teenp1.jpg',
+      nameOfPerson : 'Harry Potter',
+      timeOfJoining: 2
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+
+interface ListOfPeople {
+  profilePicture: string;
+  nameOfPerson: string;
+  timeOfJoining: number;
 }
